5b6145158b695317b474144202d10896bb607443
Dima <dima.chornous@gmail.com>
2023-10-17 19:00:52 -0600

    Edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from "./Components/First-component/FirstComponent";
 
 function App() {
   return (
       <div className="App">
-
+          <FirstComponent/>
 
       </div>
   );
---
+++ src/Components/First-component/FirstComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './FirsComponent.module.css'
+const FirstComponent = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at, consequuntur cupiditate debitis dolor et facilis
+                illum ipsam iste iure labore magnam numquam obcaecati porro praesentium recusandae, tenetur unde voluptatem?</p>
+            <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" alt=""/>
+
+
+            <hr/>
+        </div>
+    );
+};
+
+export default FirstComponent;
