1e183d34a5bbe6847f8d380b7305588e67dbc476
Dima <dima.chornous@gmail.com>
2023-10-17 19:15:26 -0600

    simpsons

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import FirstComponent from "./Components/First-component/FirstComponent";
 function App() {
   return (
       <div className="App">
-          <FirstComponent/>
+          <FirstComponent txt={'Bart'} image={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
+          <FirstComponent txt={'Homer'} image={'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
+
 
       </div>
   );
--- src/Components/First-component/FirstComponent.js
+++ src/Components/First-component/FirstComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './FirsComponent.module.css'
-const FirstComponent = () => {
+const FirstComponent = ({txt, image}) => {
+
     return (
         <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at, consequuntur cupiditate debitis dolor et facilis
-                illum ipsam iste iure labore magnam numquam obcaecati porro praesentium recusandae, tenetur unde voluptatem?</p>
-            <img src="https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png" alt=""/>
+            <h2>
+                {txt}
+            </h2>
+            <img src={image} alt=""/>
 
 
             <hr/>
